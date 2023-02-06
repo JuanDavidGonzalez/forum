c291099fec6cff5be18aaebf91fee779d788448b
Juan David Gonzalez <juan.dagore@gmail.com>
2023-02-06 15:31:07 -0500

    Add Response (Controller, Model, migration, factory, seeder)

---
+++ app/Models/Response.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'response_text',
+        'user_id',
+        'post_id'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUsernameAttribute()
+    {
+        return $this->user->name;
+    }
+}
