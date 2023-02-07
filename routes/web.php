d0cac9d4b523c6acd104dd68c332baffdc17c9c6
Juan David Gonzalez <juan.dagore@gmail.com>
2023-02-06 20:11:11 -0500

    Add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,16 @@ Auth::routes();
 Route::redirect('/', '/post');
 Route::redirect('/home', '/post');
 
-//POST
-Route::get('/post', [PostController::class, 'index'])->name('post.index');
-Route::get('/post/{post}/show', [PostController::class, 'show'])->name('post.show');
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
+//Post
+Route::prefix('post')->name('post.')->group(function () {
+Route::get('/', [PostController::class, 'index'])->name('index');
+Route::get('{post}/show', [PostController::class, 'show'])->name('show');
+Route::get('create', [PostController::class, 'create'])->name('create')->middleware('auth');
+Route::post('store', [PostController::class, 'store'])->name('store')->middleware('auth');
+});
 
-Route::get('/post/response/{post_id}', [ResponseController::class, 'create'])->name('response.create');
-Route::post('/post/response/{post_id}', [ResponseController::class, 'store'])->name('response.store');
+// Response
+Route::middleware('auth')->prefix('post')->name('response.')->group(function () {
+    Route::get('response/{post_id}', [ResponseController::class, 'create'])->name('create');
+    Route::post('response/{post_id}', [ResponseController::class, 'store'])->name('store');
+});
